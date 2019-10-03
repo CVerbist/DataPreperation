7eaa31b5b5096779f041f904202406ef667910fb
christophe <christophe216@hotmail.com>
2019-10-03 08:56:30 +0200

    Updated Main

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import numpy
+import DataPreperationFunc as dPF
 
 x = numpy.random.randint(1, 101, 50)
 y = numpy.random.randint(1, 101, 50)
 
-(newX, newY, xDel, yDel) = dataPreperation(x, y)
+(newX, newY, xDel, yDel) = dPF.dataPreperation(x, y)
 
